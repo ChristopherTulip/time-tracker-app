bfcd111ec3bee8d866bab2f5f3b537da15a84aab
Chris Tulip <Christopher.tulip@gmail.com>
2014-02-18 13:17:28 -0800

    fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeTrackerApi::Application.config.secret_key_base = '3035101536919801a2ac9dd4ba9486729ce0716f4ddcd1e2f96fa6560fe5dc17c8aebed3a95d842e4377199baee6f01b9873b105b3701eea053f163f4a9f482f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimeTrackerApi::Application.config.session_store :cookie_store, key: '_time-tracker-api_session'
