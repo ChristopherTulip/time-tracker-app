795a60ebd8d33df79dbff8a2ba752e713b8a2267
Chris Tulip <Christopher.tulip@gmail.com>
2014-02-20 10:52:22 -0800

    added json templates for the entries

---
+++ db/migrate/20140220184929_add.rb
@@ -0,0 +1,7 @@
+class Add < ActiveRecord::Migration
+  def change
+    change_table :users do |t|
+      t.string :email, null: false, default: ""
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220184002) do
+ActiveRecord::Schema.define(version: 20140220184929) do
 
   create_table "entries", force: true do |t|
     t.datetime "started_at"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140220184002) do
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",      default: "", null: false
   end
 
 end
